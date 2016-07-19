8aab6ad22e40667c212cf98999b6c8844dd963ef
Ammon Washburn <ammon.washburn@gmail.com>
2016-07-19 14:45:20 -0700

    Making repositary

---
+++ R/clinicData.R
@@ -0,0 +1,16 @@
+#' Description of clinicData
+#'
+#' I don't really know what it is but I will use it in the package
+#'
+#' @format Here are the names of the columns and my guess at what they really are \cr
+#' \strong{days_to_death} - how many days the patient survived (patient is dead) \cr
+#' \strong{days_to_last_followup} - how many days that patient has survived so far (patient is alive) \cr
+#' \strong{pathologic_grade} - How bad is this tumor \cr
+#' \strong{pathologic_stage} - How advanced is this tumor \cr
+#' \strong{sex} - Sex of patient \cr
+#' \strong{age_at_diagnosis} - Age of patient
+#'
+#'
+#' @name clinicData
+#' @source Keith Baggerly and Roger Penn
+NULL
---
+++ R/genetics.R
@@ -0,0 +1,8 @@
+#' Try to find patterns in big data genetics
+#'
+#' Explore different mutations in data
+#'
+#' @docType package
+#' @name genetics
+#'
+NULL
